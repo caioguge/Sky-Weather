88cbd62491b6c342d2dcfbc4821015b60471ffd2
Caio Guge <caioguge@gmail.com>
2023-02-15 22:36:34 -0300

    Feat: Creation of the folder structure for the Sky Weather project

---
+++ lib/views/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: const Text('Sky Weather'),
+      ),
+      body: Container(),
+    );
+  }
+}
