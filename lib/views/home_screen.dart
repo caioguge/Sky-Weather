4237f02b4df96d728e51a9696f75766130fb9285
Caio Guge <caioguge@gmail.com>
2023-06-08 10:44:53 -0300

    Feat: Customização da tela de login e definição de background image

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -6,22 +6,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 38, 100, 193),
-        elevation: 0,
-        centerTitle: true,
-        title: const Text(
-          'Sky Weather',
-          style: TextStyle(
-            fontFamily: 'Montserrat',
-            letterSpacing: -1,
-            fontWeight: FontWeight.w600,
-            fontSize: 20,
-          ),
-        ),
-      ),
-      body: const LoginScreen(),
+    return const Scaffold(
+      body: LoginScreen(),
     );
   }
 }
--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -15,93 +15,151 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(24),
-                child: TextFormField(
-                  controller: loginController,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontFamily: 'Montserrat',
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Preencha seu E-mail';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(100),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/clouds.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(24),
+                    child: TextFormField(
+                      cursorColor: Colors.black,
+                      controller: loginController,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Montserrat',
+                        color: Colors.black,
                       ),
-                    ),
-                    labelText: 'E-mail',
-                    prefixIcon: Icon(Icons.mail),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 24, left: 24, bottom: 24),
-                child: TextFormField(
-                  controller: passController,
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontFamily: 'Montserrat',
-                  ),
-                  keyboardType: TextInputType.text,
-                  obscureText: true,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Preencha sua senha';
-                    }
-                    return null;
-                  },
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(100),
-                      ),
-                    ),
-                    labelText: 'Password',
-                    prefixIcon: Icon(Icons.password),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 24, left: 24),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                content: Text('Login realizado com sucesso!')));
-                      }
-                    },
-                    child: const Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.w700,
-                          fontSize: 16,
-                          letterSpacing: -0.3,
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Preencha seu E-mail';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100),
+                          ),
+                          borderSide: BorderSide(
+                            color: Colors.white, // Cor da borda
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100),
+                          ),
+                          borderSide: BorderSide(
+                            color: Colors.white, // Cor da borda
+                          ),
+                        ),
+                        labelText: 'E-mail',
+                        labelStyle: TextStyle(color: Colors.black),
+                        prefixIcon: Icon(
+                          Icons.mail,
+                          color: Colors.white,
                         ),
                       ),
                     ),
                   ),
-                ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(right: 24, left: 24, bottom: 24),
+                    child: TextFormField(
+                      cursorColor: Colors.black,
+                      controller: passController,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Montserrat',
+                        color: Colors.black,
+                      ),
+                      keyboardType: TextInputType.text,
+                      obscureText: true,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Preencha sua senha';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100),
+                          ),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100),
+                          ),
+                          borderSide: BorderSide(
+                            color: Colors.white, // Cor da borda
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100),
+                          ),
+                          borderSide: BorderSide(
+                            color: Colors.white, // Cor da borda
+                          ),
+                        ),
+                        labelText: 'Password',
+                        labelStyle: TextStyle(color: Colors.black),
+                        prefixIcon: Icon(
+                          Icons.password,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 24, left: 24),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Login realizado com sucesso!'),
+                              ),
+                            );
+                          }
+                        },
+                        child: const Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.w700,
+                              fontSize: 16,
+                              letterSpacing: -0.3,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
