cc5bf3f7e3791cbfedcee386c262c165c6254107
Caio Guge <caioguge@gmail.com>
2023-06-12 16:17:45 -0300

    Feat: Criação de configuração do projeto Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:weather_idea/views/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
